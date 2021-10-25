6e2d4ea9e277b98003e66608f0610a4a8d133f90
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-25 17:07:40 -0500

    initial version

--- SwiftUI-Form/ContentView.swift
+++ SwiftUI-Form/ContentView.swift
@@ -1,16 +1,45 @@
-//
-//  ContentView.swift
-//  SwiftUI-Form
-//
-//  Created by R. Mark Volkmann on 10/25/21.
-//
-
 import SwiftUI
 
+enum NotifyMeAboutType {
+    case directMessages
+    case mentions
+    case anything
+}
+
+enum ProfileImageSize {
+    case small
+    case medium
+    case large
+}
+
 struct ContentView: View {
+    @State var notifyMeAbout: NotifyMeAboutType = .anything
+    @State var playNotificationSounds: Bool = false
+    @State var profileImageSize: ProfileImageSize = .small
+    @State var sendReadReceipts: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Notifications")) {
+                    Picker("Notify Me About", selection: $notifyMeAbout) {
+                        Text("Direct Messages").tag(NotifyMeAboutType.directMessages)
+                        Text("Mentions").tag(NotifyMeAboutType.mentions)
+                        Text("Anything").tag(NotifyMeAboutType.anything)
+                    }
+                    Toggle("Play notification sounds", isOn: $playNotificationSounds)
+                    Toggle("Send read receipts", isOn: $sendReadReceipts)
+                }
+                Section(header: Text("User Profiles")) {
+                    Picker("Profile Image Size", selection: $profileImageSize) {
+                        Text("Large").tag(ProfileImageSize.large)
+                        Text("Medium").tag(ProfileImageSize.medium)
+                        Text("Small").tag(ProfileImageSize.small)
+                    }
+                    Button("Clear Image Cache") {}
+                }
+            }
+        }
     }
 }
 
