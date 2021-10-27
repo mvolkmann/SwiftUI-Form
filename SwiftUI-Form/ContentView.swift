ea757826760013363c549616e0fbf3fa37415b40
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-27 10:36:50 -0500

    progress

--- SwiftUI-Form/ContentView.swift
+++ SwiftUI-Form/ContentView.swift
@@ -1,30 +1,38 @@
 //import Combine // for Just
 import SwiftUI
 
-// See https://stackoverflow.com/questions/58733003/swiftui-how-to-create-textfield-that-only-accepts-numbers/58736068#58736068
-/*
-struct NumberInput: View {
-    @Binding var text: String
-    var label: String
-    //@Binding var value: Int
-    
-    init(_ label: String, text: String) {
-        self.label = label
-        self.text = text
-    }
-
-    var body: some View {
-        TextField(label, text: $text)
-            .keyboardType(.numberPad)
-            .onReceive(Just(text)) { newValue in
-                let filtered = newValue.filter { "0123456789".contains($0) }
-                if filtered != newValue {
-                    self.text = filtered
-                }
-        }
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
-*/
+#endif
+
+// See https://stackoverflow.com/questions/58733003/swiftui-how-to-create-textfield-that-only-accepts-numbers/58736068#58736068
+/*
+ struct NumberInput: View {
+ @Binding var text: String
+ var label: String
+ //@Binding var value: Int
+ 
+ init(_ label: String, text: String) {
+ self.label = label
+ self.text = text
+ }
+ 
+ var body: some View {
+ TextField(label, text: $text)
+ .keyboardType(.numberPad)
+ .onReceive(Just(text)) { newValue in
+ let filtered = newValue.filter { "0123456789".contains($0) }
+ if filtered != newValue {
+ self.text = filtered
+ }
+ }
+ }
+ }
+ */
 
 enum ShirtSize: String, CaseIterable {
     case small
@@ -42,24 +50,27 @@ struct ContentView: View {
     @State private var birthday: Date = Date()
     @State private var favoriteColor: Color = .yellow
     //@State private var favoriteNumber: Int = 0
-    @State private var firstName = ""
     @State private var dogCount = 0
     @State private var hungry = false
-    @State private var lastName = ""
+    @State private var name = ""
     @State private var motto = "This is my motto."
     @State private var rating = 0.0
     @State private var shirtSize: ShirtSize = .large
     
     var isEditing = false
     
+    func saveUser() {
+        print("saveUser entered")
+    }
+    
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Profile")) {
-                    TextField("First Name", text: $firstName)
-                    TextField("Last Name", text: $lastName)
+                Section(header: Text("Personal")) {
+                    TextField("Name", text: $name)
                     DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
                     Toggle("Hungry?", isOn: $hungry)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
                 }
                 Section(header: Text("Preferences")) {
                     // Links work in Simulator, but not in Preview.
@@ -89,7 +100,6 @@ struct ContentView: View {
                         Stepper("# of Dogs", value: $dogCount, in: 0...10)
                         Text(String(dogCount))
                     }
-                    Button("Save") {}
                 }
                 
                 // Common UI components that are not built into SwiftUI include:
@@ -101,6 +111,20 @@ struct ContentView: View {
                 //   (supported in macOS with Picker and
                 //   .pickerStyle(RadioGroupPickerStyle())
                 // - toggle buttons: alternative is Picker
+            }
+            .navigationTitle("Profile")
+            // accentColor changes the color of many elements including
+            // the text cursor color, focus color, and Slider bar color.
+            // It does not affect the focus background color of Toggle views.
+            // Use the "toggleStyle" view modifier for that.
+            .accentColor(.red)
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button(action: self.hideKeyboard) {
+                        Image(systemName: "keyboard.chevron.compact.down")
+                    }
+                    Button("Save", action: saveUser)
+                }
             }
         }
     }
